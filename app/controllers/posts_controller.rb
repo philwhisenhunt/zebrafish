b9739c3b29e6a7ce3b11713479b780920a12bfd7
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-12-03 11:57:05 -0500

    Add post save option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,6 +62,9 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     
     respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path }
+      end
     end
   end
 
