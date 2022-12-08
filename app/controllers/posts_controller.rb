c0cca54135c9982d739e1ac827047a7621a1ee2c
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-12-07 21:42:59 -0500

    Add json for else

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,6 +68,7 @@ class PostsController < ApplicationController
       else
         format.turbo_stream { render turbo_stream: turbo_stream.replace(@post, partial: 'posts/form', locals: {post: @post }) }
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
