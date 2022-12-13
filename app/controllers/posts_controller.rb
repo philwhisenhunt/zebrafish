39ece00f6908a660d55d65ca2571d78c52271e32
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-12-13 18:00:53 -0500

    Remove old create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,20 +21,6 @@ class PostsController < ApplicationController
   def edit
   end
 
-  # POST /posts or /posts.json
-  def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
