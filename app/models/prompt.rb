04fc8baf1e0cc7c5da9134a0fe22c09901b9d37d
Phil Whisenhunt <phil.whisenhunt@slope.io>
2023-01-14 22:12:28 -0500

    Clean up comments

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -5,8 +5,7 @@ class Prompt < ApplicationRecord
     after_create_commit { broadcast_prepend_to :prompts }
 
     def check_answer(answer_attempt)
-        # system("say we did it")
-        # debugger
+      
         if self.answer == answer_attempt
             return true 
         else
