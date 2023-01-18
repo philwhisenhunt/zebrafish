57555e496be13b628c2ea33d503ef5c4601dd4db
Phil Whisenhunt <phil.whisenhunt@slope.io>
2023-01-18 17:45:24 -0500

    Run migration for status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_010758) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_224432) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.integer "likes"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_010758) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
