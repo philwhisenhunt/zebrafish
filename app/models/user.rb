bf0ed4c66da2626efd22ea0dd535db3aec678a9e
Phil Whisenhunt <phil.whisenhunt@slope.io>
2023-03-09 20:28:07 -0500

    Plan connection for users and quizzes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # do we want users to own the quizzes they make? 
+  # or would it be better to have a user both own a quiz but also subscribe to other quizzes?
 end
