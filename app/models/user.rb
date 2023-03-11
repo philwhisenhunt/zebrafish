80383ec49601998e4746063fdedf7bc04526d5c7
Phil Whisenhunt <phil.whisenhunt@slope.io>
2023-03-10 23:14:58 -0500

    Continue plan

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   # do we want users to own the quizzes they make? 
   # or would it be better to have a user both own a quiz but also subscribe to other quizzes?
+
+  # options are a join model or just starting with the basics of connecting a quiz to a creator.
+  # to add a creator we would just need a migration
 end
