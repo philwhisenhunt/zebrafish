a457f44728dc1a31f1e85cff8e7abb65019b291f
Phil Whisenhunt <phil.whisenhunt@slope.io>
2023-04-13 21:51:42 -0400

    Fix variable

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -10,7 +10,7 @@ class QuizzesController < ApplicationController
 
     # instead, show the specific quizzes that are available to a user
     # @owned_quizzes = QuizzesUsers.where(user_id: current_user.id)
-    # subscribed_quizzes = QuizzesUsers.where(subscriber_id: current_user.id)
+    # @subscribed_quizzes = QuizzesUsers.where(subscriber_id: current_user.id)
 
     
   end
