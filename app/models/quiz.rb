414f9e3a0ca6b7e795d380d8b4956d68cad3caca
Phil Whisenhunt <phil.whisenhunt@slope.io>
2023-08-03 22:33:04 -0400

    Check in plan for subscriber model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,6 +2,11 @@ class Quiz < ApplicationRecord
     has_many :prompts
     has_many :quizzes_users
     
+    # a subscriber belongs to a user
+    # a user has_one subscriber
+    # a quiz has many subscribers
+    # a subscriber has many quizzes
+    # a user has many quizzes through subscribers
 
     def activate
         self.status = "active"
