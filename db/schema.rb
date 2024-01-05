ea35dff8a7b05337456ab9898928286570515d7e
Phil Whisenhunt <phil.whisenhunt@slope.io>
2024-01-05 17:44:39 -0500

    Run update task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_020504) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_05_224357) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.integer "likes"
